b92088f4bc4d7d578e6c0228f4e4748257bac6a7
Dawid Komęza <dawid.komeza@gmail.com>
2023-01-05 18:25:28 +0000

    Initial backend configuration.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(3000, () => {});
